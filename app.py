ccd693365a863abe460e7c18ab16fb3aa4231d23
Str1xer <lavrukhinegor@yandex.ru>
2022-07-05 20:17:56 +0300

    Добавлен раздел "Настройки"; Настройки сохраняются в config.JSON; Настройки ProPresenter перенесены в config.JSON и могут настраиваться в приложении; Добавлено уведомление об Ошибке подключения к ProPresenter; Добавлена возможность отключить подключение к ProPresenter; Убрано состояние "focus" с кнопок камеры, чтобы на пробел не активировалась кнопка, по которой ранее нажимали мышкой; На JS и Python написан шаблон функии инициализации кнопок при подключении; Светлый интерфейс заменён на тёмный;

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import eel
 import PyATEMMax as ate
+import json
 
 switcher = ate.ATEMMax()
 
@@ -8,10 +9,22 @@ eel.init("web")
 switcher.connect('192.168.10.240')
 
 @eel.expose
-def connect(x):
-    switcher.connect(x)
-    print(x)
-    return True
+def init():
+    return [2, 1, 0]
+    # return [switcher.previewInput[0].videoSource.value, switcher.programInput[0].videoSource.value, 0]
+
+@eel.expose
+def saveSettings(switcher1_ip, switcher2_ip, pp_linking):
+    data = None;
+    with open('web/config.JSON') as json_file:
+        data = json.load(json_file)
+    data["Switcher 1"] = switcher1_ip
+    data["Switcher 2"] = switcher2_ip
+    data["propresenter_linking"] = pp_linking
+    json_string = json.dumps(data)
+    with open('web/config.JSON', 'w') as outfile:
+        outfile.write(json_string)
+
 
 @eel.expose
 def setProgram(x):
@@ -27,4 +40,12 @@ def setPreview(x):
     switcher.setPreviewInputVideoSource(0, int(x)+1)
 
 
-eel.start("index.html", mode='default')
+@eel.expose
+def setMixState(x):
+    print(x)
+
+
+try:
+    eel.start("index.html")
+except Exception:
+    eel.start("index.html", mode='default')
