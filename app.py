6d2125609956290d8fe3184226a318978b18d29a
Str1xer <lavrukhinegor@yandex.ru>
2022-10-16 22:26:02 +0300

    Switcher 1 and Switcher 2 can work together. Switcher 1 - Camera view. Switcher 2 - PC view. config.js deprecate and be replaced by .json

--- app.py
+++ app.py
@@ -3,58 +3,89 @@ import PyATEMMax as ate
 import json
 
 debug = False
-switcher = ate.ATEMMax()
 afk = False
 
+switcher1 = ate.ATEMMax()
+switcher2 = ate.ATEMMax()
 eel.init("web")
 
-switcher.connect('192.168.10.245')
+def getSettingsFromJson():
+    data = None
+    with open('web/config.JSON') as json_file:
+        data = json.load(json_file)
+    return data
 
 @eel.expose
 def init():
-    if (debug):
-        return [2, 1, 0]
+    if debug:
+        return [2, 1, 0, 1]
     else:
-        return [switcher.previewInput[0].videoSource.value-1, switcher.programInput[0].videoSource.value-1, 0]
+        return [switcher1.previewInput[0].videoSource.value-1,
+                switcher1.programInput[0].videoSource.value-1,
+                0,
+                switcher2.programInput[0].videoSource.value
+                ]
 
 @eel.expose
 def saveSettings(switcher1_ip, switcher2_ip, pp_linking):
-    data = None;
-    with open('web/config.JSON') as json_file:
-        data = json.load(json_file)
-    data["Switcher 1"] = switcher1_ip
-    data["Switcher 2"] = switcher2_ip
-    data["propresenter_linking"] = pp_linking
-    json_string = json.dumps(data)
-    with open('web/config.JSON', 'w') as outfile:
-        outfile.write(json_string)
+    global settings
+    try:
+        data = None
+        with open('web/config.JSON') as json_file:
+            data = json.load(json_file)
+        data["Switcher 1"] = switcher1_ip
+        data["Switcher 2"] = switcher2_ip
+        data["propresenter_linking"] = pp_linking
+        json_string = json.dumps(data)
+        with open('web/config.JSON', 'w') as outfile:
+            outfile.write(json_string)
+        settings = getSettingsFromJson()
+    except Exception:
+        print("config.json is not founded")
 
 
+# Switcher 1 functions
 @eel.expose
 def setProgram(x):
-    if switcher.connected == False:
-        switcher.connect(x)
-    switcher.setProgramInputVideoSource(0, int(x)+1)
+    if not switcher1.connected:
+        switcher1.connect(settings['Switcher 1'])
+    switcher1.setProgramInputVideoSource(0, int(x)+1)
     return
 
 @eel.expose
 def setPreview(x):
-    if switcher.connected == False:
-        switcher.connect(x)
-    switcher.setPreviewInputVideoSource(0, int(x)+1)
+    if not switcher1.connected:
+        switcher1.connect(settings['Switcher 1'])
+    switcher1.setPreviewInputVideoSource(0, int(x)+1)
 
 
 @eel.expose
 def setMixState(x):
-    if switcher.connected == False:
-        switcher.connect(x)
-    switcher.setTransitionPosition(0, int(x))
+    if not switcher1.connected:
+        switcher1.connect(settings['Switcher 1'])
+    switcher1.setTransitionPosition(0, int(x))
+
+# Switcher 2 functions
+@eel.expose
+def setProgramPC(x):
+    if not switcher2.connected:
+        switcher2.connect(settings['Switcher 2'])
+    switcher2.setProgramInputVideoSource(0, int(x)+1)
+    return
 
 @eel.expose
 def beginAFK(x):
-    if switcher.connected == False:
-        switcher.connect(x)
+    if not switcher2.connected:
+        switcher2.connect(settings['Switcher 2'])
     print(int(x))
+
+# Main
+
+settings = getSettingsFromJson()
+
+if not debug:
+    switcher1.connect(settings['Switcher 1'])
+    switcher2.connect(settings['Switcher 2'])
 
 try:
     eel.start("index.html", port=8080)
